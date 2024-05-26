99ed85abad31498323b5a52ca2c2bb939b76cdac
JadonAbhishek <121433939+JadonAbhishek@users.noreply.github.com>
2024-05-26 16:14:59 +0530

    sum of numbers from 1 to 100
    Macbook

---
+++ Integers.java
@@ -0,0 +1,21 @@
+package basic_practiceCodes;
+
+public class Integers {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+
+        // Loop to print numbers from 1 to 1000 and calculate the sum
+        for (int i = sum; i <= 100; i++) {
+            System.out.println(i);
+            sum += i;
+        }
+
+        // Print the sum of numbers from 1 to 1000
+        System.out.println("Sum of numbers from 1 to 100: " + sum);
+    }
+}
+
+
+
