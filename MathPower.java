740b507d09fbd8db799bf7d176d03363fd3c57aa
JadonAbhishek <121433939+JadonAbhishek@users.noreply.github.com>
2024-05-28 23:53:31 +0530

    MathsPower.java
    updated

--- MathPower.java
+++ MathPower.java
@@ -4,7 +4,7 @@ public class MathPower {
 
 	public static void main(String[] args) {
 		
-Scanner s= new Scanner(System.in);
+	Scanner s = new Scanner(System.in);
         
         // Prompt the user to enter a number
         System.out.print("Enter a number: ");
@@ -16,6 +16,7 @@ Scanner s= new Scanner(System.in);
         // Print powers of the number from 1st to 10th
         for (int i = 1; i <= 10; i++) {
             double power = Math.pow(number, i);
+		//prints the ith power of the number, formatted to 2 decimal places.
             System.out.printf("The %dth power of %.2f is %.2f%n", i, number, power);
 		}
 		
